6a3d68e219a190958d4c1c2f5058b71c9ca69ac7
Shujaat Ali Kulvi <shujaat.k@withrider.com>
2025-05-03 14:56:54 +0500

    Courier vs TIN Status Matrix Admin Backend

---
+++ Application/Interfaces/ICourierStatusMappingService.cs
@@ -0,0 +1,20 @@
+﻿using Application.DTOs;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface ICourierStatusMappingService
+    {
+        Task<List<CourierStatusMappingDto>> GetAllAsync();
+        Task<CourierStatusMappingDto> GetByIdAsync(int id);
+        Task<List<CourierStatusMappingDto>> GetByCIdAsync(int cid);
+        Task<bool> AddAsync(List<CreateCourierStatusMappingDto> cDto);
+        Task DeleteAsync(int id);
+        Task UpdateAsync(UpdateCourierStatusMappingDto uDto);
+    }
+}
---
+++ Infrastructure/Repositories/OrderStatusesRepository.cs
@@ -0,0 +1,52 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class OrderStatusesRepository : IOrderStatusesRepository
+    {
+        private readonly OrderDbContext _orderDbContext;
+        public OrderStatusesRepository(OrderDbContext orderDbContext)
+        {
+            _orderDbContext = orderDbContext;
+        }
+        public async Task<int> AddAsync(orderstatuses c)
+        {
+            await _orderDbContext.AddAsync(c);
+            await _orderDbContext.SaveChangesAsync();
+            int pid = c.id;
+            return pid;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var x = await _orderDbContext.orderstatuses.FindAsync(id);
+            if (x != null)
+            {
+                _orderDbContext.orderstatuses.Remove(x);
+                await _orderDbContext.SaveChangesAsync();
+            }
+        }
+
+        public async Task<List<orderstatuses>> GetAllAsync()
+        {
+            var data = await _orderDbContext.orderstatuses.ToListAsync();
+            return data;
+        }
+
+        public async Task<orderstatuses> GetAsync(int id) => await _orderDbContext.orderstatuses.FindAsync(id);
+
+        public async Task UpdateAsync(orderstatuses c)
+        {
+            _orderDbContext.orderstatuses.Update(c);
+            await _orderDbContext.SaveChangesAsync();
+        }
+    }
+}
