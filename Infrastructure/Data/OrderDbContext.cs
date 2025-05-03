45092036867233596f02345245ed03f22082ccd8
Shujaat Ali Kulvi <shujaat.k@withrider.com>
2025-05-03 15:17:25 +0500

    Courier vs TIN Status Matrix Admin Backend

--- Infrastructure/Data/OrderDbContext.cs
+++ Infrastructure/Data/OrderDbContext.cs
@@ -14,6 +14,9 @@ namespace Infrastructure.Data
         public DbSet<courierapisetting> courierapisetting { get; set; }
         public DbSet<customerpriority> customerpriority { get; set; }
         public DbSet<customerbudget> customerbudget { get; set; }
+        public DbSet<orderstatuses> orderstatuses { get; set; }
+        public DbSet<courierstatuses> courierstatuses { get; set; }
+        public DbSet<courierstatusmapping> courierstatusmapping { get; set; }
         public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
         {
 
