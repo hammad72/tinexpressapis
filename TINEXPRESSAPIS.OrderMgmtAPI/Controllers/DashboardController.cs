4b19f1c7137e3bda6d0d3297983899aa5a7f91d7
Hammad <messihammad999@gmail.com>
2025-05-06 07:21:04 +0500

    Dahboard Api Revamp

--- TINEXPRESSAPIS.OrderMgmtAPI/Controllers/DashboardController.cs
+++ TINEXPRESSAPIS.OrderMgmtAPI/Controllers/DashboardController.cs
@@ -1,5 +1,6 @@
 ﻿using Application.DTOs;
 using Application.Interfaces;
+using Application.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TINEXPRESSAPIS.OrderMgmtAPI.Controllers
@@ -15,7 +16,7 @@ namespace TINEXPRESSAPIS.OrderMgmtAPI.Controllers
             _dashboardService = dashboardService;
         }
 
-        [HttpGet]
+        [HttpGet]//Admin Dashboard
         public async Task<ActionResult<DashboardDTO>> GetDashboardData([FromQuery] string timeFilter = "monthly")
         {
             if (!IsValidTimeFilter(timeFilter))
@@ -23,14 +24,38 @@ namespace TINEXPRESSAPIS.OrderMgmtAPI.Controllers
                 return BadRequest("Invalid time filter. Valid values are: today, weekly, monthly, yearly, overall");
             }
 
-            var dashboardData = await _dashboardService.GetDashboardData(timeFilter);
+            var dashboardData = await _dashboardService.GetDashboardData(timeFilter,"Admin",null);
             return Ok(dashboardData);
+        }
+        [HttpGet("get-recent-orders")] //admin
+        public async Task<IActionResult> getRecentOrders(string timeFilter, int ordStatusID,string? barValue, int pageIndex, int pageSize)
+        {
+            var paginatedList = await _dashboardService.GetRecentOrders( timeFilter,  ordStatusID,  barValue, pageIndex, pageSize, "Admin", null);
+            return Ok(paginatedList);
         }
 
         private bool IsValidTimeFilter(string filter)
         {
             var validFilters = new[] { "today", "weekly", "monthly", "yearly", "overall" };
             return validFilters.Contains(filter.ToLower());
+        }
+
+        [HttpGet("for-customer")] //Dashboard For Customer
+        public async Task<ActionResult<DashboardDTO>> GetDashboardDataCustomer([FromQuery] int customerID, [FromQuery] string timeFilter = "monthly" )
+        {
+            if (!IsValidTimeFilter(timeFilter))
+            {//test GIT
+                return BadRequest("Invalid time filter. Valid values are: today, weekly, monthly, yearly, overall");
+            }
+
+            var dashboardData = await _dashboardService.GetDashboardData(timeFilter, "Customer",customerID);
+            return Ok(dashboardData);
+        }
+        [HttpGet("get-recent-orders-customer")] //admin
+        public async Task<IActionResult> getRecentOrders(string timeFilter, int ordStatusID, string? barValue, int pageIndex, int pageSize,int customerID)
+        {
+            var paginatedList = await _dashboardService.GetRecentOrders(timeFilter, ordStatusID, barValue, pageIndex, pageSize, "Customer", customerID);
+            return Ok(paginatedList);
         }
     }
 }
