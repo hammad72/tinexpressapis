51c43d38d4e109b575e0857412aa62008e334304
Shujaat Ali Kulvi <shujaat.k@withrider.com>
2025-05-08 15:07:01 +0500

    Signup duplication check

--- Application/Services/CustomerProfileService.cs
+++ Application/Services/CustomerProfileService.cs
@@ -21,20 +21,28 @@ namespace Application.Services
         }
         public async Task<int> AddAsync(CreateCustomerProfileDto_Ex ccp)
         {
-            CreateUserLoginsDto cUserLoginsDto = new CreateUserLoginsDto(ccp.email, "123123", ccp.user_role_id, true, "", ccp.created_by);
-            int ulid = await _ulRepository.AddAsync(_mapper.Map<userlogins>(cUserLoginsDto));
+            bool res = await _ulRepository.checkUserExist(ccp.email);
+            if (res == false)
+            {
+                CreateUserLoginsDto cUserLoginsDto = new CreateUserLoginsDto(ccp.email, "123123", ccp.user_role_id, true, "", ccp.created_by);
+                int ulid = await _ulRepository.AddAsync(_mapper.Map<userlogins>(cUserLoginsDto));
 
-            CreateCustomerProfileDto cp = new CreateCustomerProfileDto(ulid, ccp.name, ccp.legal_name, ccp.email, ccp.phone,
-                ccp.payment_method_id, ccp.referral_name, ccp.fav_pickup_address, ccp.fav_dropoff_address, ccp.address,
-                ccp.enrollment_date, ccp.business_type_id, ccp.invoice_frequency, ccp.sales_tax_num, ccp.proof_of_delivery,
-                ccp.desired_attempts, ccp.other, ccp.created_by, 1);
-            int cpid = await _cpRepository.AddAsync(_mapper.Map<customerprofile>(cp));
+                CreateCustomerProfileDto cp = new CreateCustomerProfileDto(ulid, ccp.name, ccp.legal_name, ccp.email, ccp.phone,
+                    ccp.payment_method_id, ccp.referral_name, ccp.fav_pickup_address, ccp.fav_dropoff_address, ccp.address,
+                    ccp.enrollment_date, ccp.business_type_id, ccp.invoice_frequency, ccp.sales_tax_num, ccp.proof_of_delivery,
+                    ccp.desired_attempts, ccp.other, ccp.created_by, 1);
+                int cpid = await _cpRepository.AddAsync(_mapper.Map<customerprofile>(cp));
 
-            CreateCustomerUserProfileDto cup = new CreateCustomerUserProfileDto(ulid, ccp.name, "", "",
-            ccp.email, "", ccp.enrollment_date, ccp.enrollment_date, ccp.address, "", 0,
-            ccp.phone, ccp.other, cpid, ccp.created_by, 1);
-            int upid = await _cupRepository.AddAsync(_mapper.Map<customeruserprofile>(cup));
-            return cpid;
+                CreateCustomerUserProfileDto cup = new CreateCustomerUserProfileDto(ulid, ccp.name, "", "",
+                ccp.email, "", ccp.enrollment_date, ccp.enrollment_date, ccp.address, "", 0,
+                ccp.phone, ccp.other, cpid, ccp.created_by, 1);
+                int upid = await _cupRepository.AddAsync(_mapper.Map<customeruserprofile>(cup));
+                return cpid;
+            }
+            else
+            {
+                return -5;
+            }
         }
         public async Task DeleteAsync(int id)
         {
